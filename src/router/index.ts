5178b1f8db509b1fd0e93f5ac9e7ac47562eb59a
sevenlet <ansevenlet@126.com>
2022-05-23 11:22:20 +0800

    feat: 进入页面前添加显示进度条功能

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -11,6 +13,15 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     },
   },
   build: {
+    minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
