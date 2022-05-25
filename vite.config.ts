46064763f759cf1af896b0742a86ecc9546c0dbf
sevenlet <ansevenlet@126.com>
2022-05-25 11:54:42 +0800

    build: 添加依赖，修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteCompression from 'vite-plugin-compression';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
     // gzip压缩 生产环境生成 .gz 文件
     viteCompression({
       verbose: true,
